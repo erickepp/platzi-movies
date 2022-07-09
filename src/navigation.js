146820ef10e67440a68865b4e4b5399d16dc32a3
Erick Patzan <erickestuardopatzan@gmail.com>
2022-07-09 17:11:17 -0600

    filter by category

--- src/navigation.js
+++ src/navigation.js
@@ -64,6 +64,14 @@ function categoriesPage() {
   categoriesPreviewSection.classList.add('inactive');
   genericSection.classList.remove('inactive');
   movieDetailSection.classList.add('inactive');
+
+  // ['#category', 'id-name']
+  const [_, categoryData] = location.hash.split('=');
+  const [categoryId, categoryName] = categoryData.split('-');
+
+  headerCategoryTitle.innerText = categoryName;
+  
+  getMoviesByCategory(categoryId);
 }
 
 function movieDetailsPage() {
