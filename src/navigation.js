cdaca1d0a7c6ff4855be4f8c14ad738b987dd45b
Erick Patzan <erickestuardopatzan@gmail.com>
2022-07-13 19:09:04 -0600

    movie details

--- src/navigation.js
+++ src/navigation.js
@@ -97,6 +97,10 @@ function movieDetailsPage() {
   categoriesPreviewSection.classList.add('inactive');
   genericSection.classList.add('inactive');
   movieDetailSection.classList.remove('inactive');
+
+  // ['#movie', 'id']
+  const [_, movieId] = location.hash.split('=');
+  getMovieById(movieId);
 }
 
 function searchPage() {
